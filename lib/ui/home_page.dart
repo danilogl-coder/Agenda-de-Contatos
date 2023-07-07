6dcb48c3da59d301c0e267b18f67fcf4b61a3bac
danilo <daniloglcomercial@gmail.com>
2023-07-07 13:26:38 -0300

    .

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -2,26 +2,46 @@ import "package:flutter/material.dart";
 
 import "../helpers/contac_helper.dart";
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   ContactHelper helper = ContactHelper();
 
+  List<Contact> contacts = [];
+
   @override
-  void initState() {
-    Contact c = Contact();
-    c.name = "Danilo";
-    c.email = "Danilo@gmail.com";
-    c.phone = "03249834";
-    c.img = "img.txt";
-
-    helper.saveContact(c);
-
-    helper.getAllContacts().then((list) => print(list));
+  initState() {
+    helper.getAllContacts().then((list) {
+      setState(() {
+        contacts = list;
+      });
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Contatos"),
+        backgroundColor: Colors.red,
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.white,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+        backgroundColor: Colors.red,
+      ),
+      body: ListView.builder(
+        itemBuilder: (context, index) {},
+        itemCount: contacts.length,
+        padding: EdgeInsets.all(10.0),
+      ),
+    );
   }
 }
