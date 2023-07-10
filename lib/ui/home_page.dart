ef9778226a502bd3febdd10017ee2d43b38f10c9
danilo <daniloglcomercial@gmail.com>
2023-07-10 12:58:14 -0300

    .

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,3 +1,5 @@
+import "dart:io";
+
 import "package:flutter/material.dart";
 
 import "../helpers/contac_helper.dart";
@@ -38,10 +40,59 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: Colors.red,
       ),
       body: ListView.builder(
-        itemBuilder: (context, index) {},
+        itemBuilder: (context, index) {
+          return _contactCard(context, index);
+        },
         itemCount: contacts.length,
         padding: EdgeInsets.all(10.0),
       ),
     );
+  }
+
+  Widget _contactCard(BuildContext context, int index) {
+    return GestureDetector(
+        child: Card(
+      child: Padding(
+        padding: EdgeInsets.all(10.0),
+        child: Row(children: [
+          Container(
+              width: 80.0,
+              height: 80.0,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                image: _getDecorationImage(index),
+              )),
+          Padding(
+            padding: EdgeInsets.only(left: 10.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  contacts[index].name ?? "",
+                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  contacts[index].email ?? "",
+                  style: TextStyle(fontSize: 18),
+                ),
+                Text(
+                  contacts[index].phone ?? "",
+                  style: TextStyle(fontSize: 18),
+                )
+              ],
+            ),
+          )
+        ]),
+      ),
+    ));
+  }
+
+  _getDecorationImage(index) {
+    if (contacts[index].img != null) {
+      return DecorationImage(
+          image: FileImage(File(contacts[index].img)), fit: BoxFit.cover);
+    } else {
+      return AssetImage('images/logo.png');
+    }
   }
 }
